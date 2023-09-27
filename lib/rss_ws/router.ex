9624d214e7857d3d5558f69433b17ba6bddee16f
JohnBakhmat <johnbakhmat@gmail.com>
2023-09-27 13:53:44 +0300

    made it to pattern matching

--- lib/rss_ws/router.ex
+++ lib/rss_ws/router.ex
@@ -30,33 +30,47 @@ defmodule RssWs.Router do
   end
 
   post "/open" do
-    {:ok, redis_conn} = Redix.start_link("redis://localhost:6379/3", name: :redix)
-    {ok, data} = conn.body_params |> parse_url()
+    redis_conn =
+      case Redix.start_link("redis://localhost:6379/3") do
+        {:ok, redis_conn} ->
+          redis_conn
+        {:error, {:already_started, redis_conn}} ->
+          redis_conn
+        {:error, err} ->
+          respond(conn, 500, "Error connection to Redis")
+      end
 
-    if ok == :error do
-      conn
-      |> put_resp_content_type("application/json")
-      |> send_resp(400, Poison.encode!(data))
+    conn.body_params
+    |> parse_url
+    |> case do
+      {:error, error} ->
+        respond(conn, 400, Poison.encode!(error))
+
+      {:ok, data} ->
+        Redix.command(redis_conn, ["GET", data])
+        |> case do
+          {:ok, url_cache} ->
+            IO.inspect("Cache hit")
+            respond(conn, 200, Poison.encode!(%{websocket_url: url_cache}))
+
+          {:ok, nil} ->
+            IO.inspect("Cache didnt hit")
+            ws_url = "ws://" <> data
+            Redix.command(redis_conn, ["SET", data, ws_url])
+            respond(conn, 200, Poison.encode!(%{websocket_url: ws_url}))
+
+          {_, err} ->
+            respond(conn, 500, err)
+        end
     end
 
-    {ok, url_cache} = Redix.command(redis_conn, ["GET", data])
+    conn
+  end
 
-    if ok == :ok and url_cache != nil do
-      IO.inspect("Cache hit")
-
-      conn
-      |> put_resp_content_type("application/json")
-      |> send_resp(200, Poison.encode!(%{websocket_url: url_cache}))
-    else
-      IO.inspect("Cache didnt hit")
-
-      ws_url = "ws://" <> data
-      Redix.command(redis_conn, ["SET", data, ws_url])
-
-      conn
-      |> put_resp_content_type("application/json")
-      |> send_resp(200, Poison.encode!(%{websocket_url: ws_url}))
-    end
+  defp respond(conn, status, message) do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(status, message)
   end
 
   defp parse_url(body) do
